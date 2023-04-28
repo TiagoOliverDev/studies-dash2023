cb4b1e6175cd21970c91b3eaac16f5dfecdc0f77
JackSSads <fotosjackson2018@hotmail.com>
2023-04-28 16:02:08 -0300

    fet: configuracao  do react router v6

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Cadastro } from "../pages/cadastro";
+import { Login } from "../pages/login";
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path="/cadastro" element={<Cadastro />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="*" element={<Navigate to={"/cadastro"}/>}/>
+        </Routes>
+    )
+}
