f8b2fb40575b10b4a904892dba2cbba7f8daf968
TiagoOliverDev <paulloholiveira77@gmail.com>
2023-05-10 16:18:15 -0300

    feat: up

--- src/pages/login/Form.tsx
+++ src/pages/login/Form.tsx
@@ -3,23 +3,18 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from "@mui/material/IconButton";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import GoogleIcon from '@mui/icons-material/Google';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import AppleIcon from '@mui/icons-material/Apple';
-import FacebookIcon from '@mui/icons-material/Facebook';
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HttpsIcon from '@mui/icons-material/Https';
 import Image from "./Image";
+
 
 export default function Form(props: any) {
 
@@ -38,60 +33,69 @@ export default function Form(props: any) {
     event.preventDefault();
   };
 
-
   return (
     <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "75vh"}}>
         <Image path = {path}/>
         <div style={{display: "flex", justifyContent: "center"}}>
             <div>
                 <h2 style={{color: "#FFFFFF"}} >Bem-vindo à AdaUpsoft Academy</h2>
-                <span style={{color: "#FFFFFF"}} >Novo por aqui ?</span><a style={{textDecoration: "none", color: "#530182", cursor: "pointer"}} href="http://www.google.com"><span> Crie sua conta agora!</span></a>   
-                <Box
-                  
+                <span style={{color: "#FFFFFF"}} >Novo por aqui ?</span><a style={{textDecoration: "none", color: "#530182", cursor: "pointer"}} href="/cadastro"><span> Crie sua conta agora!</span></a>   
+                <Box                 
                 >
                 <div style={{display: "flex", flexDirection: "column"}}>
-                    <label style={{marginTop: "30px", marginBottom: "14px", color: "#FFFFFF"}} >Email</label>
+                    <label style={{marginTop: "30px", marginBottom: "14px", color: "#FFFFFF"}} >E-mail</label>
                     <TextField 
                         id="email" 
                         variant="outlined" 
-                        sx={{ "& input":  { borderRadius: "17px !important", color: "#FFFFFF"}, width: "345px", height: "35px", backgroundColor: "#1F1F22 !important"}} />
+                        InputProps={{   
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <AccountCircleIcon style={{color: "#540182"}} />
+                              </InputAdornment>
+                            ),
+                          }}
+                        sx={{ 
+                            "& input":  {color: "#FFFFFF"}, 
+                            bgcolor: "#1F1F22",
+                            width: "400px", 
+                            borderRadius: "10px"                               
+                         }}
+                        />
 
                     <label style={{marginTop: "30px", color: "#FFFFFF"}} >Senha</label>
-                    <TextField
-                        style={{width: "345px"}}
-                        type={showPassword ? "text" : "password"}
-                        variant="outlined"
+                        <TextField 
+                        variant="outlined" 
+                        type={showPassword ? "text" : "password"}                          
                         margin="normal"
                         required
-                        // fullWidth
-                        id={props.id}
-                        label={props.label}
-                        name={props.name}
-                        InputProps={{
-                            sx: {
-                            bgcolor: "#1F1F22",
-                            "& input": {
-                                color: "#FFFFFF",
-                                borderRadius: "17px !important",
-                            },
-                            },
-                            endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                                >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
+                        InputProps={{   
+                            startAdornment: (
+                            <InputAdornment position="start">
+                                <HttpsIcon style={{color: "#540182"}} />
                             </InputAdornment>
                             ),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                    >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}                           
+                        sx={{ 
+                            "& input":  {color: "#FFFFFF"}, 
+                            bgcolor: "#1F1F22",
+                            width: "400px", 
+                            borderRadius: "10px"                               
                         }}
-                        />
-                    {/* <TextField id="pass" variant="outlined" sx={{ pt: 1, "& input": { borderRadius: "16px !important" }, width: "345px", height: "35px" }} /> */}
+                        
+                    />                 
                 </div>
-
                 <br/>
 
                 <Box style={{marginTop: "15px", display: 'flex', alignItems: 'center'}}>
@@ -99,42 +103,31 @@ export default function Form(props: any) {
                         <Checkbox id="keep-logged-in" {...label} defaultChecked style={{  }} />
                         Manter conectado
                     </label>
-                    <a href="http://www.google.com" style={{textDecoration: "none", color: "#530182", cursor: "pointer", marginLeft: "70px"}}><span>Esqueci minha senha</span></a>
+                    <a href="http://www.google.com" style={{textDecoration: "none", color: "#530182", cursor: "pointer", marginLeft: "75px"}}><span>Esqueci minha senha</span></a>
                 </Box>
-
 
                 </Box>
                 <Stack spacing={2} direction="row">
-                  <Button style={{borderRadius: 17, width: "345px", height: "35px", marginTop: "29px"}} variant="contained">Entrar</Button>
+                  <Button style={{borderRadius: 17, width: "400px", height: "40px", marginTop: "29px"}} variant="contained">Entrar</Button>
                 </Stack>
-
-                <Box sx={{ position: "relative", width: "100%", maxWidth: 360, bgcolor: "background.paper", marginTop: "31px"  }}>
-                    <Divider  variant="middle" />
-                    <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", background: "#0B0B0B", color: "#FFFFFF" }}>
+                <Box style={{ marginTop: "31px", display: 'flex', justifyContent: "center", alignItems: "center", color: "#FFFFFF"}}>
+                    <Box sx={{width: "30%", height: "1px", bgcolor: "gray"}} />
+                        <Box sx={{margin: "0 10px"}}>
                         Ou faça login com
-                    </Box>
-                </Box>
+                        </Box>
+                    <Box sx={{width: "30%", height: "1px", bgcolor: "gray"}} />
+                </Box>      
                 <Box style={{marginTop: "28px", display:"flex", justifyContent:"center", alignItems:"center" }} >
                     <Button style={{background: "#1F1F22", margin: "0 10px"}} >
                         <GoogleIcon />
-                    </Button>
-                    <Button style={{background: "#1F1F22", margin: "0 10px" }} >
-                        <InstagramIcon />
-                    </Button>
-                    <Button style={{background: "#1F1F22", margin: "0 10px" }} >
-                        <LinkedInIcon />
-                    </Button>
-                   
+                    </Button>              
                 </Box>
                 <Box style={{marginTop: "28px", display:"flex", justifyContent:"center", alignItems:"center" }} >                  
                     <Button style={{background: "#1F1F22", margin: "0 10px" }} >
                         <GitHubIcon />
                     </Button>
                     <Button style={{background: "#1F1F22", margin: "0 10px" }} >
-                        <AppleIcon />
-                    </Button>
-                    <Button style={{background: "#1F1F22", margin: "0 10px" }} >
-                        <FacebookIcon />
+                        <LinkedInIcon />
                     </Button>
                 </Box>
             </div>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-// import GridRegister from "../../shared/components/GridRegister/GridRegister";
-import Image from "./Image";
 import Form from "./Form";
-// import '../../stylesGlobal.css';
 
 export default function Index() {
-
-  let path = "/images/baner.svg"
-
+ 
   return (
     <div style={{background: "#0B0B0B", height: "100%" }} >
-      {/* <Image path = {path}/> */}
       <Form/>
     </div>
   );
